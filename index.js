48108df50ea4e4555bfda689bbe6cdb0d81d64b6
lassev05 <vestergaardlasse2@gmail.com>
2022-01-23 21:05:59 +0100

    update versions

---
+++ index.js
@@ -0,0 +1,7 @@
+const versions = ["v0.1.1"];
+const mode = ["production", "development", "demo"];
+
+// Language: javascript
+// Path: index.js
+module.exports.versions = versions;
+module.exports.mode = mode;
