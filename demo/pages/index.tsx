aacff104e1ce5d26979346c34a3b0ca211c6948b
lassev05 <vestergaardlasse2@gmail.com>
2022-03-02 20:44:12 +0100

    New updateon ui

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -3,9 +3,14 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
     return (
         <>
-            <h1 className="text-center underline decoration-lime-600">
-                Hello world from tailwind!
-            </h1>
+            <div className="bg-gray-200 min-h-screen text-center flex flex-col justify-center items-center">
+                <h1 className="text-gray-700 mb-2 font-mono">
+                    We are currently working on a new demo!
+                </h1>
+                <p className="text-[10px] text-gray-800 font-light">
+                    Please check back later.
+                </p>
+            </div>
         </>
     );
 };
