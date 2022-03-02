2b33c83134e93948eadf9748efe9177b8efd773e
lassev05 <vestergaardlasse2@gmail.com>
2022-03-02 19:42:48 +0100

    Add next js to demo

---
+++ demo/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <h1 className="text-center underline decoration-lime-600">
+                Hello world from tailwind!
+            </h1>
+        </>
+    );
+};
+
+export default Home;
--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,20 +1,10 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
-        extend: {
-            colors: {
-                primary: "#313139",
-                primaryGray: "#7E7E7E",
-                primaryBorder: "#9F9F9F",
-                primaryBorderBg: "#E9E9E9",
-                primaryButton: "#6C6E75",
-                primaryButtonHover: "#5f6169",
-            },
-
-            fontFamily: {
-                body: ["Popins", "sans-serif"],
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
