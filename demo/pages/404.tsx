715373739f8ab795cd10774446eca4933b3fba09
Lasse Vestergaard <77295879+lassv@users.noreply.github.com>
2022-03-03 14:34:49 +0100

    edit the bg off 404.tsx The new color: bg-gray-200

--- demo/pages/404.tsx
+++ demo/pages/404.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const NotFound: NextPage = () => {
     return (
-        <div className="bg-gray-800 text-center justify-center items-center min-h-screen flex flex-col">
+        <div className="bg-gray-200 text-center justify-center items-center min-h-screen flex flex-col">
             <h3 className="text-gray-200 mb-2">
                 Sorry but the page you are looking for does not exist.
             </h3>
