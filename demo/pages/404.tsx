0630ac4d39a4ac257bf8d147c23730b6535385db
Lasse Vestergaard <77295879+lassv@users.noreply.github.com>
2022-03-03 12:18:13 +0100

    Edited a button on 404.tsx

--- demo/pages/404.tsx
+++ demo/pages/404.tsx
@@ -8,7 +8,7 @@ const NotFound: NextPage = () => {
                 Sorry but the page you are looking for does not exist.
             </h3>
             <Link href="/">
-                <a className="px-3 py-2 bg-neutral-300 text-sm rounded hover:bg-neutral-100 shadow-lg focus:ring">
+                <a className="px-3 py-2 bg-sky-500 text-sm rounded hover:bg-sky-400 shadow-lg focus:ring">
                     Return Home
                 </a>
             </Link>
